65436b290b6ccebedebec8d6603f69ca5492ca0b
minheonjun <tamunavollt1@gmail.com>
2023-09-25 21:54:58 +0900

    2023-09-25 실습과제#5 3번

---
+++ main.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+let input = [];
+
+rl.question("이름을 입력하세요:", (ans) => {
+    input.push(ans);
+    rl.question("학번을 입력하세요.", (ans) => {
+        input.push(ans);
+        rl.close();
+    });
+});
+
+rl.on("close", (line) => {
+    console.log("\n<출력>\n");
+    input.map((line) => console.log(line));
+    process.exit();
+});
